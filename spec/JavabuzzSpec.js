5a9c95898fe34b123b2fef6b47b9bf2669d83a83
paulwallis42 <paulwallis42@gmail.com>
2015-04-13 16:26:19 +0100

    passing Java test

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -2,21 +2,22 @@ describe('Javabuzz', function() {
 
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
 
     it('divisible by 5', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by 15', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
@@ -25,20 +26,24 @@ describe('Javabuzz', function() {
   describe('knows when a number is NOT', function() {
 
     it('not divisible by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(1)).toBe(false);
     });
 
     it('not divisible by 5', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(7)).toBe(false);
     });
 
     it('not divisible by 15',  function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFifteen(6)).toBe(false);
     });
 
+  });
+
+  describe('when playing, says', function() {
+
+    it('"Java" when a number is divisible by 3', function() {
+      expect(javabuzz.says(3)).toEqual("Java");
+    });
   });
 
 });
